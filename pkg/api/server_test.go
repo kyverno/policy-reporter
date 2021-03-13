345cc9014408b4998008d9fe9306604fc9bf60a3
Frank Jogeleit <fj@move-elevator.de>
2021-03-13 19:41:19 +0100

    add new server test

---
+++ pkg/api/server_test.go
@@ -0,0 +1,22 @@
+package api_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/fjogeleit/policy-reporter/pkg/api"
+	"github.com/fjogeleit/policy-reporter/pkg/report"
+	"github.com/fjogeleit/policy-reporter/pkg/target"
+	"github.com/fjogeleit/policy-reporter/pkg/target/loki"
+)
+
+func Test_NewServer(t *testing.T) {
+	server := api.NewServer(
+		report.NewPolicyReportStore(),
+		report.NewClusterPolicyReportStore(),
+		[]target.Client{loki.NewClient("http://localhost:3100", "debug", true, &http.Client{})},
+		8080,
+	)
+
+	go server.Start()
+}
