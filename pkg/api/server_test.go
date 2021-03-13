b62da873fec152ed89269ed859c57a8d6fe4e307
Frank Jogeleit <fj@move-elevator.de>
2021-03-13 19:53:20 +0100

    add test case

--- pkg/api/server_test.go
+++ pkg/api/server_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/fjogeleit/policy-reporter/pkg/api"
 	"github.com/fjogeleit/policy-reporter/pkg/report"
 	"github.com/fjogeleit/policy-reporter/pkg/target"
+	"github.com/fjogeleit/policy-reporter/pkg/target/discord"
 	"github.com/fjogeleit/policy-reporter/pkg/target/loki"
 )
 
@@ -14,7 +15,10 @@ func Test_NewServer(t *testing.T) {
 	server := api.NewServer(
 		report.NewPolicyReportStore(),
 		report.NewClusterPolicyReportStore(),
-		[]target.Client{loki.NewClient("http://localhost:3100", "debug", true, &http.Client{})},
+		[]target.Client{
+			loki.NewClient("http://localhost:3100", "debug", true, &http.Client{}),
+			discord.NewClient("http://webhook:2000", "", false, &http.Client{}),
+		},
 		8080,
 	)
 
