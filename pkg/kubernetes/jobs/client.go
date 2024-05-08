e1dcd218482a9990d64cd5b9d03a547582849ec7
Frank Jogeleit <frank.jogeleit@lovoo.com>
2024-05-08 12:17:14 +0200

    batch pushes
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- pkg/kubernetes/jobs/client.go
+++ pkg/kubernetes/jobs/client.go
@@ -3,7 +3,6 @@ package jobs
 import (
 	"context"
 
-	gocache "github.com/patrickmn/go-cache"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,29 +17,16 @@ type Client interface {
 
 type k8sClient struct {
 	client v1.BatchV1Interface
-	cache  *gocache.Cache
 }
 
 func (c *k8sClient) Get(scope *corev1.ObjectReference) (*batchv1.Job, error) {
-	if cached, ok := c.cache.Get(string(scope.UID)); ok {
-		return cached.(*batchv1.Job), nil
-	}
-
-	pod, err := kubernetes.Retry(func() (*batchv1.Job, error) {
+	return kubernetes.Retry(func() (*batchv1.Job, error) {
 		return c.client.Jobs(scope.Namespace).Get(context.Background(), scope.Name, metav1.GetOptions{})
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	c.cache.Set(string(scope.UID), pod, 0)
-
-	return pod, nil
 }
 
-func NewClient(client v1.BatchV1Interface, cache *gocache.Cache) Client {
+func NewClient(client v1.BatchV1Interface) Client {
 	return &k8sClient{
 		client: client,
-		cache:  cache,
 	}
 }
--- pkg/kubernetes/pods/client.go
+++ pkg/kubernetes/pods/client.go
@@ -3,7 +3,6 @@ package pods
 import (
 	"context"
 
-	gocache "github.com/patrickmn/go-cache"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -17,29 +16,16 @@ type Client interface {
 
 type k8sClient struct {
 	client v1.CoreV1Interface
-	cache  *gocache.Cache
 }
 
 func (c *k8sClient) Get(scope *corev1.ObjectReference) (*corev1.Pod, error) {
-	if cached, ok := c.cache.Get(string(scope.UID)); ok {
-		return cached.(*corev1.Pod), nil
-	}
-
-	pod, err := kubernetes.Retry(func() (*corev1.Pod, error) {
+	return kubernetes.Retry(func() (*corev1.Pod, error) {
 		return c.client.Pods(scope.Namespace).Get(context.Background(), scope.Name, metav1.GetOptions{})
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	c.cache.Set(string(scope.UID), pod, 0)
-
-	return pod, nil
 }
 
-func NewClient(client v1.CoreV1Interface, cache *gocache.Cache) Client {
+func NewClient(client v1.CoreV1Interface) Client {
 	return &k8sClient{
 		client: client,
-		cache:  cache,
 	}
 }
