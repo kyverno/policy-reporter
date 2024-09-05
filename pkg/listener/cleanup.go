2dfa0bd59eb0c1046a23d1228798ed3d98207e26
Frank Jogeleit <frank.jogeleit@web.de>
2024-09-05 20:04:14 +0200

    rework security hub synchronization and cleanup
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- pkg/listener/cleanup.go
+++ pkg/listener/cleanup.go
@@ -3,6 +3,8 @@ package listener
 import (
 	"context"
 
+	"github.com/kyverno/policy-reporter/pkg/crd/api/policyreport/v1alpha2"
+	"github.com/kyverno/policy-reporter/pkg/helper"
 	"github.com/kyverno/policy-reporter/pkg/report"
 	"github.com/kyverno/policy-reporter/pkg/target"
 )
@@ -12,6 +14,16 @@ const CleanUpListener = "cleanup_listener"
 func NewCleanupListener(ctx context.Context, targets *target.Collection) report.PolicyReportListener {
 	return func(event report.LifecycleEvent) {
 		for _, handler := range targets.Clients() {
+			if event.Type != report.Deleted {
+				filtered := helper.Filter(event.PolicyReport.GetResults(), func(result v1alpha2.PolicyReportResult) bool {
+					return handler.Validate(event.PolicyReport, result)
+				})
+
+				if len(filtered) == 0 {
+					return
+				}
+			}
+
 			handler.CleanUp(ctx, event.PolicyReport)
 		}
 	}
