52e3da944107de3c89c428115678b5fab5e7e593
Frank Jogeleit <frank.jogeleit@web.de>
2024-11-28 12:35:28 +0100

    feat(api): Support LabelSelector in,exist and notexist operation
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- pkg/kubernetes/namespaces/client.go
+++ pkg/kubernetes/namespaces/client.go
@@ -2,10 +2,14 @@ package namespaces
 
 import (
 	"context"
+	"strings"
 
 	gocache "github.com/patrickmn/go-cache"
+	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 
 	"github.com/kyverno/policy-reporter/pkg/helper"
@@ -22,13 +26,38 @@ type k8sClient struct {
 }
 
 func (c *k8sClient) List(ctx context.Context, selector map[string]string) ([]string, error) {
-	labelSelector := metav1.FormatLabelSelector(&metav1.LabelSelector{MatchLabels: selector})
-	if cached, ok := c.cache.Get(labelSelector); ok {
+	s := labels.NewSelector()
+	for l, v := range selector {
+		var err error
+		var req *labels.Requirement
+
+		if strings.Contains(v, ",") {
+			req, err = labels.NewRequirement(l, selection.In, helper.Map(strings.Split(v, ","), func(val string) string {
+				return strings.TrimSpace(val)
+			}))
+		} else if v == "*" {
+			req, err = labels.NewRequirement(l, selection.Exists, nil)
+		} else if v == "!*" {
+			req, err = labels.NewRequirement(l, selection.DoesNotExist, nil)
+		} else {
+			req, err = labels.NewRequirement(l, selection.Equals, []string{v})
+		}
+		if err != nil {
+			zap.L().Error("failed to create selector requirement", zap.Error(err), zap.String("label", l), zap.String("value", v))
+			continue
+		}
+
+		s = s.Add(*req)
+	}
+
+	zap.L().Debug("created label selector for namespace resolution", zap.String("selector", s.String()))
+
+	if cached, ok := c.cache.Get(s.String()); ok {
 		return cached.([]string), nil
 	}
 
 	list, err := kubernetes.Retry(func() ([]string, error) {
-		namespaces, err := c.client.List(ctx, metav1.ListOptions{LabelSelector: labelSelector})
+		namespaces, err := c.client.List(ctx, metav1.ListOptions{LabelSelector: s.String()})
 		if err != nil {
 			return nil, err
 		}
@@ -41,7 +70,7 @@ func (c *k8sClient) List(ctx context.Context, selector map[string]string) ([]str
 		return nil, err
 	}
 
-	c.cache.Set(labelSelector, list, 0)
+	c.cache.Set(s.String(), list, 0)
 
 	return list, nil
 }
