2cafe9955f101181c1cb18142798196842129a99
Nader Debbabi <nader.debbabi.30@gmail.com>
2025-10-29 12:06:34 -0400

    Fix AlertManager integration test
    Signed-off-by: Nader Debbabi <nader.debbabi.30@gmail.com>

--- test/alertmanager/main.go
+++ test/alertmanager/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/openreports/reports-api/apis/openreports.io/v1alpha1"
 
 	"github.com/kyverno/policy-reporter/pkg/openreports"
@@ -13,6 +15,16 @@ import (
 )
 
 func main() {
+
+	// Create a partial test report
+	report := v1alpha1.Report{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-report",
+			Namespace: "test-namespace",
+		},
+	}
+
 	// Create a test result
 	result := v1alpha1.ReportResult{
 		Description: "Test policy violation from manual test",
@@ -48,7 +60,7 @@ func main() {
 
 	// Send the test alert
 	fmt.Println("Sending test alert to", alertManagerURL)
-	client.Send(&openreports.ReportAdapter{Report: &v1alpha1.Report{}}, openreports.ResultAdapter{ReportResult: result})
+	client.Send(&openreports.ReportAdapter{Report: &report}, openreports.ResultAdapter{ReportResult: result})
 
 	// Also test batch send
 	results := []openreports.ResultAdapter{
@@ -75,7 +87,7 @@ func main() {
 	}
 
 	fmt.Println("Sending batch test alerts")
-	client.BatchSend(nil, results)
+	client.BatchSend(&openreports.ReportAdapter{Report: &report}, results)
 
 	// Wait a moment to ensure alerts are sent before the program exits
 	time.Sleep(500 * time.Millisecond)
